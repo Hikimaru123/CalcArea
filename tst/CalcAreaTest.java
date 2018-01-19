8f52575bc5c44dd277380db182aed12122541c9d
asci02 <higashiyama-k105r@ascsys.onmicrosoft.com>
2018-01-19 16:57:18 +0900

    例文のテストケースを作成

--- tst/CalcAreaTest.java
+++ tst/CalcAreaTest.java
@@ -1,3 +1,4 @@
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -5,8 +6,18 @@ import org.junit.Test;
 public class CalcAreaTest {
 
 	@Test
-	public void test() {
-		fail("まだ実装されていません");
+	public void calcTriangleTest底辺10cm_高さ5cmで25cm2() {
+		double actual	= CalcArea.calcTriangleArea(10,5);
+		double expected	 = 25;
+		assertThat(actual,is(expected));
+	}
+
+
+	@Test
+	public void calcCircleTes半径5cmで78_5cm2() {
+		double actual	= CalcArea.calcCircleArea(5);
+		double expected	 = 78.5;
+		assertThat(actual,is(expected));
 	}
 
 }
